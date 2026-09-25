7f3fd92363d3f75ae9d44fc166500ec63f0e62fd
dry700 <tjdrdx01@gmail.com>
2026-09-26 00:08:26 +0700

    chore: remove check_mask.gd

--- check_mask.gd
+++
@@ -1,6 +0,0 @@
-extends SceneTree
-func _init():
-    var player_scene = load("res://scenes/player/player.tscn")
-    var player = player_scene.instantiate()
-    print("Player mask: ", player.collision_mask)
-    quit()
