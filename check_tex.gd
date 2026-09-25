37dfe6d2cf2c4c8c04ae2b6458c04d5ef57109c6
dry700 <tjdrdx01@gmail.com>
2026-09-25 23:19:13 +0700

    chore: remove check_tex.gd

--- check_tex.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-func _init():
-    var tex = load("res://assets/sprites/weapons/hoa_dagger.png")
-    print("Dagger size: ", tex.get_size())
-    tex = load("res://assets/sprites/weapons/thuy_spear.png")
-    print("Spear size: ", tex.get_size())
-    quit()
